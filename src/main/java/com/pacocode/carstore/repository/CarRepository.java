557690baf1476788dd1fb75e28ae7ef534836a7a
Amílcar Paco <amilcarcpaco@gmail.com>
2022-08-29 14:21:56 +0200

    add: findCarByMotorCode method

--- src/main/java/com/pacocode/carstore/repository/CarRepository.java
+++ src/main/java/com/pacocode/carstore/repository/CarRepository.java
@@ -3,7 +3,9 @@ package com.pacocode.carstore.repository;
 import com.pacocode.carstore.model.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CarRepository extends JpaRepository<Car, UUID> {
+    Optional<Car> findCarByMotorCode(String motorCode);
 }
